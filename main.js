a87ed44892d38d1fe4e559f7cebb3efcd35cd9b1
Akbar <sulajmankulovakbar@gmail.com>
2023-09-27 20:13:35 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+// 1 задание 
+// function factorialRecursive(n) {  
+
+//     if (n === 0) {
+//         return 1;
+//     } else {
+//         return n * factorialRecursive(n - 1);
+//     }
+//     }
+    
+    
+    
+//     const number = 5;
+//     const factorial = factorialRecursive(number);
+//     console.log(`Факториал ${number} равен ${factorial}`);
+    ////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+// 2 задание 
+//     const text = "My name 457 is Alex";
+// const string = text.match(/\D/g);
+
+// if (string) {
+// const result = string.join("");
+// console.log(result);
+// } else {
+// console.log("В строке нет символов, не являющихся цифрами.");}
+
+/////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+// 3 задание
+// const text = "Hello\tWorld\nTest";
+// const whitespaceCharacters = text.match(/\s/g);
+
+// if (whitespaceCharacters) {
+// const result = whitespaceCharacters.join("");
+// console.log(result);
+// } 
+/////////////////////////////////////////////////////////////////////////////
+
+
+
+// 4 доп задание
+// function countChar(str, char) {
+//     let count = 0;
+//     for (let i = 0; i < str.length; i++) {
+//     if (str[i] === char) {
+//         count++;
+//     }
+//     }
+//     return count;
+// }
+
+// const inputString = 'loremipsumdolor';
+// const characterToCount = 'o';
+// const result = countChar(inputString, characterToCount);
+
+// console.log(result); 
